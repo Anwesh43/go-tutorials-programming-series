82177c3a365f15ace1eb898f638d31ade4b57969
Anwesh43 <anweshthecool0@gmail.com>
2017-02-24 16:00:02 +0530

    continue and break

--- loops.go
+++ loops.go
@@ -5,12 +5,24 @@ import "fmt"
 func main() {
 	sum := 0
 	for i := 0; i < 10; i++ {
+		if (i+1)%2 == 0 {
+			continue
+		}
 		fmt.Println(i + 1)
 		sum = sum + (i + 1)
+		if i+1 == 5 {
+			break
+		}
 	}
 	a := make([]int, 10)
 	for i := 0; i < 10; i++ {
+		if (i+1)%2 == 1 {
+			continue
+		}
 		a[i] = (i + 1) * 2
+		if i+1 == 5 {
+			break
+		}
 	}
 	fmt.Println(a)
 	fmt.Println(sum)
