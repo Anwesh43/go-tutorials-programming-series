594105a22c00aff04fc690636ad7f94e716543fe
Anwesh43 <anweshthecool0@gmail.com>
2017-02-24 12:56:02 +0530

    Adding conditional statements

---
+++ conditionals_demo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 20
+	if a == 12 {
+		fmt.Println("a is equalto 12")
+	} else if a == 10 {
+		fmt.Println("a is eqaulto 10")
+	} else {
+		fmt.Println("a is neither 10 nor 12")
+	}
+}
